cea33f3ffcb5daf7db790fd54e5539655d489f9d
Jeff Powell <powellj@mac.com>
2012-02-21 09:15:28 -0700

    Added Edit & Destroy links to roles partial, added roles edit page

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -21,6 +21,40 @@ class RolesController < ApplicationController
    end
   end
 
+  def edit
+    @role = project.roles.find(params[:id])
+    respond_with [project, @role]
+  end
+
+  def update
+    @role = project.roles.find(params[:id])
+    update_was_successful = @role.update_attributes(params[:role])
+
+    respond_with [project, @role] do |format| # redirects to url: /projects/1/roles/1
+     format.html {
+       if update_was_successful
+         redirect_to(project_path(project), :notice => 'The role was saved')
+       else
+         render 'edit'
+       end
+     }
+   end
+  end
+
+  def delete
+    role = project.roles.find(params[:id])
+    if role.destroy
+      flash[:notice] = 'The role was destroyed'
+    end
+      flash[:alert] = 'The role could not be destroyed'
+    end
+    respond_with [project, @role] do |format|
+      format.html {
+        redirect_to(project_path(project))
+      }
+    end
+  end
+
   private
 
   def project
