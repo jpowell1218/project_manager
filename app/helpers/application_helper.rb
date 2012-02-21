80654e6875b7d8138e921b82708ced0d7078c32a
Jeff Powell <powellj@mac.com>
2012-02-20 17:54:07 -0700

    Refinement to link_to_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def link_to_icon(icon_name, url_or_object, options={})
+    options.merge!({ :class => "icon #{icon_name}" })
     link_to(image_tag("icons/#{icon_name}.png"),
               url_or_object,
               options)
